480b033734c3fcf86143d58817b86170fe46d1c4
Bit Second <bitsecondal@gmail.com>
2017-10-07 07:55:07 -0500

    Added timestamp and logging

--- sites/all/modules/custom/speedtest-cli/performspeedtest.sh
+++ sites/all/modules/custom/speedtest-cli/performspeedtest.sh
@@ -1,9 +1,13 @@
 #!/bin/bash 
 
 function main {
+	echo $(date) | "Running internet test"
+
 	FEEDNAME=$(date +%Y%m%d%H%M%S).csv
 	# /usr/bin/python $HOME/bin/speedtest-cli/speedtest.py --csv-header --csv-delimiter "|" > ${HOME}/intranetp/feeds/inetspeed/${FEEDNAME}
 	/usr/bin/python $HOME/bin/speedtest-cli/speedtest.py --csv --csv-delimiter "|" >> ${HOME}/intranetp/feeds/inetspeed/${FEEDNAME}
+
+	echo $(date) | "Done running internet test"
 }
 # end function main 
 
