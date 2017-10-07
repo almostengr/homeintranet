814bcf5f27d9bb6b73be7f5d3618788eb5c11fc8
Bit Second <bitsecondal@gmail.com>
2017-10-06 23:52:18 -0500

    Removed header from csv file

--- sites/all/modules/custom/speedtest-cli/performspeedtest.sh
+++ sites/all/modules/custom/speedtest-cli/performspeedtest.sh
@@ -1,8 +1,9 @@
 #!/bin/bash 
 
 function main {
-	# /usr/bin/python /home/aphintranet/bin/speedtest-cli/speedtest.py --csv-header --csv-delimiter "|" > ${FEEDFILE}
-	/usr/bin/python $HOME/bin/speedtest-cli/speedtest.py --csv --csv-delimiter "|" >> ${HOME}/intranetp/feeds/inetspeed/$(date +%Y%m%d%H%M%S).csv
+	FEEDNAME=$(date +%Y%m%d%H%M%S).csv
+	# /usr/bin/python $HOME/bin/speedtest-cli/speedtest.py --csv-header --csv-delimiter "|" > ${HOME}/intranetp/feeds/inetspeed/${FEEDNAME}
+	/usr/bin/python $HOME/bin/speedtest-cli/speedtest.py --csv --csv-delimiter "|" >> ${HOME}/intranetp/feeds/inetspeed/${FEEDNAME}
 }
 # end function main 
 
